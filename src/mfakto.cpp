0239daa4beefda9f5cd7f0d98b1aa4845362fcf2
brubsby <brubsbybrubsby@gmail.com>
2024-08-21 13:05:44 -0400

    address various PR feedback

--- src/mfakto.cpp
+++ src/mfakto.cpp
@@ -136,7 +136,7 @@ int init_CLstreams(int gs_reinit_only)
         printf("ERROR: malloc(h_ktab[%d]) failed\n", i);
         return 1;
       }
-      memset(mystuff.h_ktab[i], 0, sizeof(mystuff.h_ktab[i]));
+      memset(mystuff.h_ktab[i], 0, sizeof(*mystuff.h_ktab[i]));
       mystuff.d_ktab[i] = clCreateBuffer(context,
                         CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
                         mystuff.threads_per_grid * sizeof(cl_uint),
@@ -153,7 +153,7 @@ int init_CLstreams(int gs_reinit_only)
       printf("ERROR: malloc(h_RES) failed\n");
       return 1;
     }
-    memset(mystuff.h_RES, 0, sizeof(mystuff.h_RES));
+    memset(mystuff.h_RES, 0, sizeof(*mystuff.h_RES));
     mystuff.d_RES = clCreateBuffer(context,
                       CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
                       32 * sizeof(cl_uint),
