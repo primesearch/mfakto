c551a816b85fb181aaf830b8560c04ab03a05a14
Mingye Wang <arthur200126@gmail.com>
2025-11-09 01:08:29 +0800

    Add * before and after pattern in fnmatch
    And folks, this is why you test. Or at least let your code settle around in your brain for a bit.
    
    Many AMD Linux patterns can actually be prefix-matches (drop the * at the beginning) but let's not disturb that for now.

--- src/myfnmatch.h
+++ src/myfnmatch.h
@@ -8,19 +8,20 @@
 extern "C" {
 #endif
 
-/*
-Define our own. It would be easier to just use NTDLL's RtlIsNameInExpression()
-on Windows, but ntdll.lib is only available in the Native API for Windows 10
-and above.
-
-However, there are many GPL-compatible fnmatch implementations available.
-*/
+/* Define our own glob-matcher.
+ * NTDLL has a RtlIsNameInExpression(), but it incurs some requirements on
+ * SDK version (not on OS version) due to import lib availability.
+ * We choose to use an GPL-compatible fnmatch implementation, which also
+ * provides more features such as character ranges from POSIX glob format. */
 #define FNM_CASEFOLD   1
 #define FNM_IGNORECASE FNM_CASEFOLD
 #define FNM_NOMATCH    -1
 int fnmatch(const char *pattern, const char *string, int flags);
 #endif
 
+/* fnmatch wrapper for use as boolean. 
+ * Remember: fnmatch() matches the whole string, so add "*" before and after
+ * as needed! */
 inline static int patmatch(const char *pattern, const char *string, int flags)
 {
     return fnmatch(pattern, string, flags) == 0;
